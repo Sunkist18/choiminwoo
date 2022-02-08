899fb357547c41ea8d98c0ba3a6a7018b5b7f5c3
Sunkist18 <dandalf@o.cnu.ac.kr>
2022-02-08 23:15:52 +0900

    003 Component 사용하기

--- src/R003_ImportComponent.js
+++ src/R003_ImportComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class R003_ImportComponent extends Component {
+class R003_ImportComponent extends Component {
   render() {
     return (
       <h2>[THIS IS IMPORTED COMPONENT]</h2>
