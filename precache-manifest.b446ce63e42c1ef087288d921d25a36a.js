0f11f7f69a4f84a6e4b869caca7b787c8e92fdb4
Sunkist18 <dandalf@o.cnu.ac.kr>
2022-02-08 12:27:36 +0900

    Updates

---
+++ precache-manifest.b446ce63e42c1ef087288d921d25a36a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0560d6c2ad3aa8ec881c45d41547f29b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6d5b52de45b4cedd836",
+    "url": "/static/css/main.a9827d45.chunk.css"
+  },
+  {
+    "revision": "edee6119deb16130b3b4",
+    "url": "/static/js/2.4bd31288.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.4bd31288.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6d5b52de45b4cedd836",
+    "url": "/static/js/main.91c7d2be.chunk.js"
+  },
+  {
+    "revision": "35f8c825269832c4afea",
+    "url": "/static/js/runtime-main.141dd7e4.js"
+  }
+]);
