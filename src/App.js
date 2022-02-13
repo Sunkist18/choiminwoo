78bb9e0730c6ee2c367581146b782b8435d5b9b0
Sunkist18 <dandalf@o.cnu.ac.kr>
2022-02-13 23:27:27 +0900

    [008] 생명주기 함수 shouldComponentUpdate() 사용하기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import './App.css';
-import LifecycleEx from './R007_LifecycleEx';
+import LifecycleEx from './R008_LifecycleEx';
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>LifecycleEx 적용하기</p>
-      <LifecycleEx prop_value = 'FromApp.js' />
+      <LifecycleEx
+        prop_value="FromApp"
+      />
     </div>
   );
 }
