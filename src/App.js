704d7564ea1ea78c429608c806507f2930103da8
Sunkist18 <dandalf@o.cnu.ac.kr>
2022-02-14 14:30:24 +0900

    [015] map() 함수 사용하기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import './App.css';
-import ForEach from './R014_ForEach';
+import Map from './R015_Map';
 
 function App() {
   return (
-    <div>
+    <div> 
       <h1>Start React 200!</h1>
-      <p>ForEach 적용하기</p>
-      <ForEach />
+      <p>Map 적용하기</p>
+      <Map />
     </div>
   );
 }
