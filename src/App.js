a5cb61da39e313b7c1157c6fa2716ff76fd61dc2
Sunkist18 <dandalf@o.cnu.ac.kr>
2022-02-14 11:04:03 +0900

    [013] 화살표 함수 사용

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import './App.css';
-import ClassPrototype from './R012_Class&Prototype.js';
+import ArrowFunc from './R013_ArrowFunction.js';
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
-      <p>ClassPrototype 적용하기</p>
-      <ClassPrototype />
+      <p>ArrowFunc 적용하기</p>
+      <ArrowFunc />
     </div>
   );
 }
